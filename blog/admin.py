a67c4aa36a1a1e349a5ef0af60b452f68816af63
lilyforman <lily.forman@yahoo.com>
2017-07-19 14:27:56 -0700

    comment attempt 5

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
+from .models import Post, Comment
+
+admin.site.register(Post)
+admin.site.register(Comment)
+
+
+
+from django.contrib import admin
 from .models import Post
 # Register your models here.
 admin.site.register(Post)
